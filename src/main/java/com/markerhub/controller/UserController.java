d8857bcdf56acb1a8589bcc6e9362b44ce0cca8e
钟婉儿 <1403585646@qq.com>
2022-06-16 15:40:00 +0800

    项目搭建完成

---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.markerhub.controller;
+
+
+import com.markerhub.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2022-06-16
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/index")
+    public Object index(){
+        return userService.getById(1L);
+    }
+}
